c83fae1851133f6c03510d51e3d36a80ca9b06f1
unknown <minjun3021@naver.com>
2019-07-20 03:21:55 +0900

    camera gg

--- app/src/main/java/com/kmj/sunrinton19/AddFragment.java
+++ app/src/main/java/com/kmj/sunrinton19/AddFragment.java
@@ -1,13 +1,27 @@
 package com.kmj.sunrinton19;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class AddFragment extends Fragment {
+    FloatingActionButton floatingActionButton;
+    MainActivity mainActivity;
 
 
     @Override
@@ -20,10 +34,56 @@ public class AddFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_add, container, false);
+        mainActivity = (MainActivity) getActivity();
 
+        floatingActionButton = v.findViewById(R.id.main_addbtn);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//            sendTakePhotoIntent();
+                mainActivity.sendTakePhotoIntent();
+
+            }
+        });
 
         return v;
     }
 
-
+//    private void sendTakePhotoIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(mainActivity.getPackageManager()) != null) {
+//            File photoFile = null;
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//                // Error occurred while creating the File
+//            }
+//
+//
+//            if (photoFile != null) {
+//                photoUri = FileProvider.getUriForFile(mainActivity, "com.kmj.sunrinton19", photoFile);
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//            }
+//        }
+//    }
+//
+//    private File createImageFile() throws IOException {
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "TEST_" + timeStamp + "_";
+//        File storageDir = mainActivity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File image = File.createTempFile(
+//                imageFileName,      /* prefix */
+//                ".jpg",         /* suffix */
+//                storageDir          /* directory */
+//        );
+//        imageFilePath = image.getAbsolutePath();
+//        return image;
+//    }
+//
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        Log.e("path", imageFilePath);
+//    }
 }
--- app/src/main/java/com/kmj/sunrinton19/MainActivity.java
+++ app/src/main/java/com/kmj/sunrinton19/MainActivity.java
@@ -1,17 +1,30 @@
 package com.kmj.sunrinton19;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Fragment> fragments;
@@ -21,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
     AddFragment addFragment;
     RoomFragment roomFragment;
     ConstraintLayout ic1, ic2;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 672;
+    public static String imageFilePath;
+    private Uri photoUri;
+
+    private float aceVal;
+    private float aceLast;
+    private float shake;
+    private int cnt = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +55,21 @@ public class MainActivity extends AppCompatActivity {
         iv2=findViewById(R.id.main_bottom_ic2);
         bottom_tv1=findViewById(R.id.main_bottom_tv1);
         bottom_tv2=findViewById(R.id.main_bottom_tv2);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        300);
+            }
+        }
+
+
 
         addFragment = new AddFragment();
         roomFragment = new RoomFragment();
@@ -70,4 +108,38 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "TEST_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,      /* prefix */
+                ".jpg",         /* suffix */
+                storageDir          /* directory */
+        );
+        imageFilePath = image.getAbsolutePath();
+        return image;
+    }
+    public  void sendTakePhotoIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+
+            if (photoFile != null) {
+                 Log.e("pacc",getPackageName());
+                 Log.e("photo",photoFile.getPath());
+
+                photoUri = FileProvider.getUriForFile(this, "com.kmj.sunrinton19.provider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
 }
